b0b3bb0aa5ec107e8fefcbfd36d0d28729075d95
Dinda Nur Afifah <92873925+akabanelay@users.noreply.github.com>
2023-03-07 22:37:29 +0800

    Update Posttest1PBO.java

--- Posttest1PBO.java
+++ Posttest1PBO.java
@@ -25,11 +25,13 @@ public class Posttest1PBO {
             
             switch(pilihan){
             case 1:
-                System.out.println("==================DATA ASSET===================");
+                System.out.println("=======================================================================================");
+                System.out.println("                                         DATA ASSET                                    ");
                 hr.tampil();
                 break;
             case 2:
-                System.out.println("==================TAMBAH DATA==================");
+                System.out.println("=======================================================================================");
+                System.out.println("                                        TAMBAH DATA                                    ");
                 System.out.print("ID ASSET:");
                 int IdAsset = input.nextInt();
                 input.nextLine();
@@ -47,26 +49,28 @@ public class Posttest1PBO {
                 hr.add(data);
                 break;
             case 3:
-                System.out.println("===================UBAH DATA===================");
+                System.out.println("=======================================================================================");
+                System.out.println("                                        UBAH DATA                                      ");
                 hr.tampil();
                 System.out.print("enter ID ASSET: ");
                 int rdId = input.nextInt();
                 hr.update(rdId, input);
                 break;
             case 4 :
-                System.out.println("===================HAPUS DATA==================");
+                System.out.println("=======================================================================================");
+                System.out.println("                                          HAPUS DATA                                   ");
                 hr.tampil();
                 System.out.print("enter ID ASSET: ");
                 int dId = input.nextInt();
                 hr.delete(dId);
                 break;
             case 5 :
-                System.out.println("===============================================");
-                System.out.println("                 Keluar Sistem...              ");
+                System.out.println("=======================================================================================");
+                System.out.println("                                         Keluar Sistem...                              ");
                 System.exit(0);
             default:
-                System.out.println("===============================================");
-                System.out.println("              Pilhan Tidak Tersedia            ");
+                System.out.println("=======================================================================================");
+                System.out.println("                                    Pilhan Tidak Tersedia                              ");
                 break;
             }
         
@@ -77,13 +81,13 @@ public class Posttest1PBO {
     
     public static void menu(){
         
-        System.out.println("=====================MENU======================");
-        System.out.println("=             [1] Lihat Asset                 =");
-        System.out.println("=             [2] Tambah Asset                =");
-        System.out.println("=             [3] Ubah Asset                  =");
-        System.out.println("=             [4] Hapus Asset                 =");
-        System.out.println("=             [5] Keluar Sistem               =");
-        System.out.println("===============================================");
+        System.out.println("===========================================MENU========================================");
+        System.out.println("=                         [1] Lihat Asset                 =");
+        System.out.println("=                         [2] Tambah Asset                =");
+        System.out.println("=                         [3] Ubah Asset                  =");
+        System.out.println("=                         [4] Hapus Asset                 =");
+        System.out.println("=                         [5] Keluar Sistem               =");
+        System.out.println("=======================================================================================");
         
         System.out.println("Pilih Menu: [1/2/3/4/5]");
         
